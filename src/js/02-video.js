eb2e0d8e7e8f718b6a1b4fd278580414dff32e53
Sergey180989 <133609830+Sergey180989@users.noreply.github.com>
2023-10-02 01:47:14 +0300

    create-02-video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player('vimeo-player');
+
+player.setCurrentTime(localStorage.getItem('currentTime') || 0)
+ 
+
+player.on('timeupdate', throttle(onPlaying, 1000));
+
+function onPlaying() {
+  player
+    .getCurrentTime()
+    .then(seconds => {
+      localStorage.setItem('currentTime', seconds);
+    })
+    
+}
 
